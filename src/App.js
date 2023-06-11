c758d9b1706f04582654b79c3afcb5113c063394
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-11 01:40:30 -0500

    got started by importing in all the files that will be using the header and footer components in the App.js file and also made the page links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import Portfolio from "./Pages/Portfolio"
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import Resume from "./Pages/Resume"
+
 
 function App() {
+  const [ currPage, setCurrPage ] = useState("home")
+  const [ prefix, setPrefix ] = useState("")
+  
+  useEffect(() => {
+    if( process.env.NODE_ENV === "production" ){
+      setPrefix("/react-portfolio")
+    }
+  }, [process.env.NODE_ENV])
+
+  useEffect(() => {
+    const path = window.location.href.split(window.location.host)[1]
+    console.log(path)
+    setCurrPage(path)
+  }, [window.location.href])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header prefix={prefix} />
+
+      { currPage === `${prefix}/` && <About /> }
+      { currPage === `${prefix}/about` && <About /> }
+      { currPage === `${prefix}/portfolio` && <Portfolio /> }
+      { currPage === `${prefix}/contact` && <Contact /> }
+      { currPage === `${prefix}/resume` && <Resume /> }
+
+      <Footer prefix={prefix} />
     </div>
   );
 }
+
 
 export default App;
