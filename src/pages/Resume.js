474e296f848a2c69eae31edc3ec9d8109a559fcb
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-11 03:33:20 -0500

    started on the resume.js file

---
+++ src/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Resume.css";
+
+
+function Resume() {
+  return (
+    <div className="resume">
+      <h1> Resume </h1>
+    </div>
+  );
+}
+
+export default Resume;
